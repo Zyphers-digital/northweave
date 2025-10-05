5c9d25aa7e01de66e9e3f0c3c55fdd8022f3de64
Zyphers-digital <fjelde95@hotmail.com>
2025-10-06 01:13:06 +0200

    Fix SidebarMenu case (Linux-safe)

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react";
-import "./SidebarMenu.css";
+import "./SideBarMenu.css";
 
 export default function SidebarMenu() {
   const [open, setOpen] = useState(false);
